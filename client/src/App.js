19516a833c666c26711b48a4e722fb7d58726331
Mohamed Mansour <mohamed28mansour@gmail.com>
2020-10-02 09:47:58 +0200

    relocate seeds

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
+import UserDashboard from './components/UserDashboard';
 
 class App extends Component {
 
@@ -33,6 +34,12 @@ class App extends Component {
           exact
           path='/login'
           render={props => <Login setUser={this.setUser} {...props} />}
+        />
+        <Route
+          exact
+          path='/userdashboard'
+          render={props => <Login setUser={this.setUser} {...props} />}
+          component={UserDashboard}
         />
       </div>
     );
