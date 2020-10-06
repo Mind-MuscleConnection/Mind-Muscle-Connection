a4e148211fed2b77e02aa0c28a6dd92c12ec2777
Olesya Steinmetz <newsabome@icloud.com>
2020-10-06 12:08:50 +0200

    PlanDetails styling

--- client/src/components/PlanDetails.js
+++ client/src/components/PlanDetails.js
@@ -1,35 +1,79 @@
-import React from 'react'
+import React from 'react';
+import './PlanDetails.css'
 
 export default function PlanDetails(props) {
 
   let planDetails = props.templates.filter(template => {
     return template._id === props.templateID
   })
+
   let days = Object.keys(planDetails[0]).filter(key => key.includes('day'))
   console.log(days)
   console.log(planDetails)
   let dayDetails = days.map(day => planDetails[0][day])
   console.log(dayDetails)
   return (
-    <div>
-      {dayDetails.slice(0, 6).map((day,i) => {
+    <div className='container'>
+      <div className='flex-container'>
+      {props.numberOfDays == 3 &&
+      dayDetails.slice(0, 3).map((day,i) => {
         return (
-          <>
-          <h2>day{i+1}:</h2>
+        <div>
+          <h3 className='day'>Day{i+1}:</h3>
         {day.map(exercise => {
           return (
-            <div>
-            <h3>{exercise.name}</h3>
-            {exercise.set1goal? <h2>{exercise.name == "Run" ? "Goal: "+ exercise.set1goal+" m" : "Set 1: "+exercise.set1goal}</h2> : <></>}
-            {exercise.set2goal?  <h2>Set 2: {exercise.set2goal}</h2>: <></>}
-            {exercise.set3goal?  <h2>Set 3: {exercise.set3goal}</h2>: <></>}
+            <div className='exercise'>
+            <h6>{exercise.name}</h6>
+            {exercise.set1goal? <p>{exercise.name == "Run" ? "Goal: "+ exercise.set1goal+" m" : "Set 1: "+exercise.set1goal}</p> : <></>}
+            {exercise.set2goal?  <p>Set 2: {exercise.set2goal}</p>: <></>}
+            {exercise.set3goal?  <p>Set 3: {exercise.set3goal}</p>: <></>}
             </div>
           )
         })}
-        <br/>
-        </>
+        </div>
         )
-      })}
+      })
+    }
+    {props.numberOfDays == 5 &&
+      dayDetails.slice(0, 5).map((day,i) => {
+        return (
+        <div>
+          <h3 className='day'>Day{i+1}:</h3>
+        {day.map(exercise => {
+          return (
+            <div className='exercise'>
+            <h6>{exercise.name}</h6>
+            {exercise.set1goal? <p>{exercise.name == "Run" ? "Goal: "+ exercise.set1goal+" m" : "Set 1: "+exercise.set1goal}</p> : <></>}
+            {exercise.set2goal?  <p>Set 2: {exercise.set2goal}</p>: <></>}
+            {exercise.set3goal?  <p>Set 3: {exercise.set3goal}</p>: <></>}
+            </div>
+          )
+        })}
+        </div>
+        )
+      })
+    }
+    {props.numberOfDays == 6 &&
+      dayDetails.slice(0, 6).map((day,i) => {
+        return (
+        <div>
+          <h3 className='day'>Day{i+1}:</h3>
+        {day.map(exercise => {
+          return (
+            <div className='exercise'>
+            <h6>{exercise.name}</h6>
+            {exercise.set1goal? <p>{exercise.name == "Run" ? "Goal: "+ exercise.set1goal+" m" : "Set 1: "+exercise.set1goal}</p> : <></>}
+            {exercise.set2goal?  <p>Set 2: {exercise.set2goal}</p>: <></>}
+            {exercise.set3goal?  <p>Set 3: {exercise.set3goal}</p>: <></>}
+            </div>
+          )
+        })}
+        </div>
+        )
+      })
+    }
+      </div>
+      <button className='button-pick'>Pick this plan</button>
     </div>
   )
 }
